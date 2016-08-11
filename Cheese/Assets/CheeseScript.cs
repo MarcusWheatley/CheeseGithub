8b203570a5af3e3f57f5a93f925d70d3bc36a350
Marcus Wheatley <marcus.wheatley@ad.aie.edu>
2016-08-11 10:23:25 +0930

    Added debug.log ("I am greatest B")

--- Cheese/Assets/CheeseScript.cs
+++ Cheese/Assets/CheeseScript.cs
@@ -5,7 +5,9 @@ public class CheeseScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		bool userB = true;
+		if (userB)
+			Debug.Log ("I am cxool");
 	}
 	
 	// Update is called once per frame
